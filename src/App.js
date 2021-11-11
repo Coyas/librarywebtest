3b2b6e74d720dd812031de229b5a781f5dda02e9
Ailton Duarte <adidas.coyas@gmail.com>
2021-11-11 21:30:20 -0100

    checkpoint

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+// import { ApolloProvider } from 'react-apollo';
 
 // componentes
 import BookList from './components/BookList';
+import AddBook from './components/AddBook';
 
-function App() {
-	return (
-		<div className="App">
-			<h1>TerraSystem library teste</h1>
-			<BookList />
-		</div>
-	);
+// apollo cliente setup
+const client = new ApolloClient({
+	uri: 'http://localhost:4000/graphql'
+});
+
+class App extends Component {
+	render() {
+		return (
+			<ApolloProvider client={client}>
+				<div className="main">
+					<h1>TerraSystem library teste</h1>
+					<BookList />
+					<AddBook />
+				</div>
+			</ApolloProvider>
+		);
+	}
 }
 
 export default App;
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
+// make grapgql queries
+const getBooksQuery = gql`
+	{
+		books {
+			name
+			genre
+			id
+			author {
+				name
+				age
+			}
+		}
+	}
+`;
 
 class BookList extends Component {
+	displayBooks() {
+		let data = this.props.data;
+		if (data.loading) {
+			return <div>Loading books...</div>;
+		} else {
+			return data.books.map(book => {
+				return (
+					<li key={book.id}>
+						{book.name} : {book.genre} : {book.author.name} : {book.author.age}
+					</li>
+				);
+			});
+		}
+	}
 	render() {
+		console.log(this.props);
 		return (
 			<div>
-				<ul id="book-list">
-					<li>Book Name</li>
-				</ul>
+				<ul id="book-list">{this.displayBooks()}</ul>
 			</div>
 		);
 	}
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
